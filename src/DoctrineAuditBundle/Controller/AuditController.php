8eb6b5ca7758a33084ffe8d6d6b41730f0fd9af7
Gregoire <klesk44@gmail.com>
2018-11-16 16:57:32 +0100

    Run php-cs-fixer

--- src/DoctrineAuditBundle/Controller/AuditController.php
+++ src/DoctrineAuditBundle/Controller/AuditController.php
@@ -2,9 +2,9 @@
 
 namespace DH\DoctrineAuditBundle\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AuditController extends Controller
 {
