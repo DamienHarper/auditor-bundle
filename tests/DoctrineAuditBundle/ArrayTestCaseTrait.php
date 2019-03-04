adc7c214ce9d2d1598d33b234dc2fe8218c3fc9a
damienharper <damien.harper@gmail.com>
2019-03-04 10:44:50 +0100

    Refactoring

--- tests/DoctrineAuditBundle/ArrayTestCaseTrait.php
+++ tests/DoctrineAuditBundle/ArrayTestCaseTrait.php
@@ -13,12 +13,12 @@ trait ArrayTestCaseTrait
      * @param array $expected
      * @param array $array
      */
-    protected function assertArraySimilar(array $expected, array $array)
+    protected function assertArraySimilar(array $expected, array $array): void
     {
-        $this->assertTrue(count(array_diff_key($array, $expected)) === 0);
+        $this->assertCount(0, array_diff_key($array, $expected));
 
         foreach ($expected as $key => $value) {
-            if (is_array($value)) {
+            if (\is_array($value)) {
                 $this->assertArraySimilar($value, $array[$key]);
             } else {
                 $this->assertContains($value, $array);
