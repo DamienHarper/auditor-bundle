c9a55ac8a22efb25ef5490b4ba219821e2381eb4
damienharper <damien.harper@gmail.com>
2019-03-16 00:32:28 +0100

    Non numeric primary key support: AuditEntry::object_id is a string (PR #37)

---
+++ tests/DoctrineAuditBundle/Fixtures/Issues/Locale.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace DH\DoctrineAuditBundle\Tests\Fixtures\Issues;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="locale")
+ */
+class Locale
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="string", length=5)
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * Get the value of id.
+     *
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id.
+     *
+     * @param string $id
+     *
+     * @return Locale
+     */
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name.
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name.
+     *
+     * @param string $name
+     *
+     * @return Locale
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function __sleep()
+    {
+        return ['id', 'name'];
+    }
+}
