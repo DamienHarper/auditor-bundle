7a02222862956985b66767bd37046604f7a77f3c
Maxime Helias <maximehelias16@gmail.com>
2019-09-11 18:06:54 +0200

    Remove deprecated and manage the compatibility (#84)

--- src/DoctrineAuditBundle/User/TokenStorageUserProvider.php
+++ src/DoctrineAuditBundle/User/TokenStorageUserProvider.php
@@ -2,6 +2,7 @@
 
 namespace DH\DoctrineAuditBundle\User;
 
+use Symfony\Component\Security\Core\Authentication\Token\SwitchUserToken;
 use Symfony\Component\Security\Core\Role\SwitchUserRole;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface as BaseUserInterface;
@@ -34,11 +35,16 @@ class TokenStorageUserProvider implements UserProviderInterface
             $impersonation = '';
             if ($this->security->isGranted('ROLE_PREVIOUS_ADMIN')) {
                 $impersonatorUser = null;
-                foreach ($this->security->getToken()->getRoles() as $role) {
-                    if ($role instanceof SwitchUserRole) {
-                        $impersonatorUser = $role->getSource()->getUser();
+                // Symfony > 4.3
+                if ($token instanceof SwitchUserToken) {
+                    $impersonatorUser = $token->getOriginalToken()->getUser();
+                } else {
+                    foreach ($this->security->getToken()->getRoles() as $role) {
+                        if ($role instanceof SwitchUserRole) {
+                            $impersonatorUser = $role->getSource()->getUser();
 
-                        break;
+                            break;
+                        }
                     }
                 }
 
