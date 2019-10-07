6c10238db92c2992e8249b18db45055c02e5bbcd
Gregoire <klesk44@gmail.com>
2019-10-07 16:56:12 +0200

    Revert proposal

---
+++ src/DoctrineAuditBundle/AuditEntry.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace DH\DoctrineAuditBundle;
+
+class AuditEntry
+{
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var string
+     */
+    protected $type;
+    /**
+     * @var int
+     */
+    protected $object_id;
+    /**
+     * @var string
+     */
+    protected $diffs;
+    /**
+     * @var int
+     */
+    protected $blame_id;
+    /**
+     * @var string
+     */
+    protected $blame_user;
+    /**
+     * @var string
+     */
+    protected $ip;
+    /**
+     * @var string
+     */
+    protected $created_at;
+
+    public function __set($name, $value)
+    {
+        $this->{$name} = $value;
+    }
+
+    /**
+     * Get the value of id.
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of type.
+     *
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Get the value of object_id.
+     *
+     * @return int
+     */
+    public function getObjectId(): ?int
+    {
+        return $this->object_id;
+    }
+
+    /**
+     * Get the value of blame_id.
+     *
+     * @return int|string
+     */
+    public function getUserId()
+    {
+        return $this->blame_id ?? 'Unknown';
+    }
+
+    /**
+     * Get the value of blame_user.
+     *
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->blame_user ?? 'Unknown';
+    }
+
+    /**
+     * Get the value of ip.
+     *
+     * @return string
+     */
+    public function getIp(): ?string
+    {
+        return $this->ip;
+    }
+
+    /**
+     * Get the value of created_at.
+     *
+     * @return string
+     */
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Get the value of created_at.
+     *
+     * @return array
+     */
+    public function getDiffs(): ?array
+    {
+        return json_decode($this->diffs, true);
+    }
+}
--- src/DoctrineAuditBundle/Controller/AuditController.php
+++ src/DoctrineAuditBundle/Controller/AuditController.php
@@ -3,6 +3,7 @@
 namespace DH\DoctrineAuditBundle\Controller;
 
 use DH\DoctrineAuditBundle\Helper\AuditHelper;
+use DH\DoctrineAuditBundle\Reader\AuditEntry;
 use DH\DoctrineAuditBundle\Reader\AuditReader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,6 +22,45 @@ class AuditController extends AbstractController
         return $this->render('@DHDoctrineAudit/Audit/audits.html.twig', [
             'audited' => $reader->getEntities(),
             'reader' => $reader,
+        ]);
+    }
+
+    /**
+     * @Route("/audit/revert/{id}/{object}/{entity}/{field}", name="dh_doctrine_audit_revert")
+     * @param $id
+     * @param $object
+     * @param $entity
+     * @param $field
+     *
+     * @return Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function revertEntityHistoryAction($id, $object, $entity, $field)
+    {
+        // get audit reader service
+        $reader = $this->container->get('dh_doctrine_audit.reader');
+        $am = $this->container->get('dh_doctrine_audit.manager');
+        // get audit entity manager
+        $em = $this->container->get('doctrine.orm.default_entity_manager');
+
+        /** @var AuditEntry $entity_audit */
+        $entity_audit = $reader->getAudit(AuditHelper::paramToNamespace($entity), $id);
+        $audited_entity = AuditHelper::paramToNamespace($entity);
+        $current_entity = $em->getRepository($audited_entity)->find($object);
+
+        // Get all differences
+        $diffs = $entity_audit[0]->getDiffs();
+        // get field value to revert
+        $field_value = $diffs[$field]['old'];
+
+        $setMethod = "set{$field}";
+
+        $current_entity->$setMethod($field_value);
+
+        $em->persist($current_entity);
+        $em->flush();
+
+        return $this->redirectToRoute('dh_doctrine_audit_show_entity_history', [
+            'entity' => $entity
         ]);
     }
 
