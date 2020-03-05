ba4fcb978e3a25727f6b35f93d7e3f68fe37a906
Henry Clifford <h.a.clifford@gmail.com>
2020-03-05 14:03:21 +0100

    handle bizarre version clash, allways use symfony component event interface

--- src/DoctrineAuditBundle/Event/AuditEvent.php
+++ src/DoctrineAuditBundle/Event/AuditEvent.php
@@ -3,48 +3,24 @@
 namespace DH\DoctrineAuditBundle\Event;
 
 use Symfony\Component\EventDispatcher\Event as ComponentEvent;
-use Symfony\Contracts\EventDispatcher\Event as ContractsEvent;
 
-if (class_exists(ContractsEvent::class)) {
-    abstract class AuditEvent extends ContractsEvent
+abstract class AuditEvent extends ComponentEvent
+{
+    /**
+     * @var array
+     */
+    private $payload;
+
+    public function __construct(array $payload)
     {
-        /**
-         * @var array
-         */
-        private $payload;
-
-        public function __construct(array $payload)
-        {
-            $this->payload = $payload;
-        }
-
-        /**
-         * @return array
-         */
-        final public function getPayload(): array
-        {
-            return $this->payload;
-        }
+        $this->payload = $payload;
     }
-} else {
-    abstract class AuditEvent extends ComponentEvent
+
+    /**
+     * @return array
+     */
+    final public function getPayload(): array
     {
-        /**
-         * @var array
-         */
-        private $payload;
-
-        public function __construct(array $payload)
-        {
-            $this->payload = $payload;
-        }
-
-        /**
-         * @return array
-         */
-        final public function getPayload(): array
-        {
-            return $this->payload;
-        }
+        return $this->payload;
     }
 }
