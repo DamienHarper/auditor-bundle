96bc3c128c64a9bbe0ad7bf04f2b464aa2d0f8ae
Sander Verkuil <sander@legionelladossier.com>
2022-03-30 23:26:01 +0200

    Add a console user provider
    Also write some tests regarding the console user provider, to verify
    that it actually is working.
    
    Fixes #267

---
+++ tests/App/Command/CreatePostCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DH\AuditorBundle\Tests\App\Command;
+
+use DateTimeImmutable;
+use DH\Auditor\Provider\Doctrine\DoctrineProvider;
+use DH\Auditor\Tests\Provider\Doctrine\Fixtures\Entity\Standard\Blog\Post;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpKernel\Kernel;
+
+class CreatePostCommand extends Command
+{
+    protected static $defaultName = 'app:post:create';
+    private DoctrineProvider $doctrineProvider;
+
+    public function __construct(DoctrineProvider $doctrineProvider)
+    {
+        parent::__construct(self::$defaultName);
+        $this->doctrineProvider = $doctrineProvider;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->write('Creating a new post');
+
+        $post = new Post();
+        $post
+            ->setTitle('Blameable post')
+            ->setBody('yet another post')
+            ->setCreatedAt(new DateTimeImmutable('2020-01-17 22:17:34'))
+        ;
+
+        $this->doctrineProvider->getAuditingServiceForEntity(Post::class)->getEntityManager()->persist($post);
+        $this->doctrineProvider->getAuditingServiceForEntity(Post::class)->getEntityManager()->flush();
+
+        // Symfony 4 compatibility
+        if (Kernel::MAJOR_VERSION >= 5) {
+            return self::SUCCESS;
+        }
+
+        return 0;
+    }
+}
