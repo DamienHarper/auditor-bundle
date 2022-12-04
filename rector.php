461a9d0d16afadc4905770bde1d4ec9cdb5d3e91
Damien Harper <damien.harper@gmail.com>
2022-12-04 22:23:29 +0100

    Rector: DoctrineSetList::DOCTRINE_DBAL_30

--- rector.php
+++ rector.php
@@ -53,7 +53,7 @@ return static function (RectorConfig $rectorConfig): void {
     // Doctrine rules
     $rectorConfig->sets([
         DoctrineSetList::DOCTRINE_CODE_QUALITY,
-//        DoctrineSetList::DOCTRINE_DBAL_30,
+        DoctrineSetList::DOCTRINE_DBAL_30,
 //        DoctrineSetList::DOCTRINE_ORM_29,
 //        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
     ]);
