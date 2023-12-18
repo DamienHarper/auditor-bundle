2f97dba6eb68d030c4172db83796dce2eb7c4c87
Oskar Stark <oskarstark@googlemail.com>
2023-12-18 12:43:44 +0100

    -

--- src/Controller/ViewerController.php
+++ src/Controller/ViewerController.php
@@ -10,7 +10,6 @@ use DH\Auditor\Provider\Doctrine\Persistence\Reader\Reader;
 use DH\Auditor\Provider\Doctrine\Persistence\Schema\SchemaManager;
 use DH\Auditor\Provider\Doctrine\Service\AuditingService;
 use DH\AuditorBundle\Helper\UrlHelper;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,7 +18,7 @@ use Twig\Environment;
 /**
  * @see \DH\AuditorBundle\Tests\Controller\ViewerControllerTest
  */
-class ViewerController extends AbstractController
+class ViewerController
 {
     private $environment;
 
