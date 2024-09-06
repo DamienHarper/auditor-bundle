c63232f24d7a99e8efffd3ef19e6edee8e117337
Damien Harper <damien.harper@gmail.com>
2024-09-06 17:19:53 +0200

    Same Rector config as `damienharper/auditor`

--- rector.php
+++ rector.php
@@ -3,63 +3,39 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
 use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Rector\Class_\MakeCommandLazyRector;
 use Rector\Symfony\Set\SymfonySetList;
-use Rector\Transform\Rector\Attribute\AttributeKeyToClassConstFetchRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddArrayParamDocTypeRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddArrayReturnDocTypeRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([__DIR__.'/src', __DIR__.'/tests']);
-
-    // Do not try to change simple property init and assign to constructor promotion
-    // to make code easier to read (no more class with properties declared both
-    // at the start of the class and in the constructor)
-    $rectorConfig->skip([
-        ClassPropertyAssignToConstructorPromotionRector::class,
-        RemoveUnusedPrivatePropertyRector::class,
-        AttributeKeyToClassConstFetchRector::class,
-        MakeCommandLazyRector::class,
-        AddArrayReturnDocTypeRector::class,
-        AddArrayParamDocTypeRector::class,
+return RectorConfig::configure()
+    ->withPaths([__DIR__.'/src', __DIR__.'/tests'])
+    ->withSkip([
         __DIR__.'/tests/App/var/*',
-    ]);
-
-    // PHP rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_80,
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::CODING_STYLE,
-        SetList::TYPE_DECLARATION,
-    ]);
-
-    // Symfony rules
-    $rectorConfig->sets([
+    ])
+    ->withPhpSets(php82: true)
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        privatization: true,
+        instanceOf: true,
+        earlyReturn: true,
+        phpunitCodeQuality: true,
+        doctrineCodeQuality: true,
+        symfonyCodeQuality: true,
+        phpunit: true,
+    )
+    ->withSets([
         SymfonySetList::SYMFONY_54,
-        SymfonySetList::SYMFONY_CODE_QUALITY,
-        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
-    ]);
-
-    // Doctrine rules
-    $rectorConfig->sets([
+        PHPUnitSetList::PHPUNIT_110,
         DoctrineSetList::DOCTRINE_CODE_QUALITY,
-        DoctrineSetList::DOCTRINE_DBAL_30,
         DoctrineSetList::DOCTRINE_ORM_29,
-        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
-    ]);
-
-    // PHPUnit rules
-    $rectorConfig->sets([
-        PHPUnitSetList::PHPUNIT_91,
-        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-        PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER,
-    ]);
-};
+        DoctrineSetList::DOCTRINE_DBAL_40,
+    ])
+    ->withAttributesSets(
+        symfony: true,
+        doctrine: true,
+        phpunit: true,
+    )
+;
