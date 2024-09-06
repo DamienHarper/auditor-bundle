ee65df839d21eb23a76dc7729286c0c93564a779
Damien Harper <damien.harper@gmail.com>
2024-09-06 15:22:39 +0200

    Same PHP-CS-Fixer config as `damienharper/auditor`

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+return (new Config())
+    ->setParallelConfig(ParallelConfigFactory::detect()) // @TODO 4.0 no need to call this manually :poop:
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PHP82Migration' => true,
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        '@DoctrineAnnotation' => true,
+        '@PhpCsFixer' => true,
+        '@PhpCsFixer:risky' => true,
+        '@PHPUnit100Migration:risky' => true,
+//        'date_time_immutable' => true,
+        'general_phpdoc_annotation_remove' => [
+            'annotations' => [
+                'expectedDeprecation',
+                'expectedException',
+                'expectedExceptionMessage',
+                'expectedExceptionMessageRegExp',
+            ],
+        ],
+        'ordered_interfaces' => true,
+        'ordered_traits' => true,
+        'phpdoc_to_param_type' => true,
+        'phpdoc_to_property_type' => true,
+        'phpdoc_to_return_type' => true,
+        'regular_callable_call' => true,
+        'simplified_if_return' => true,
+        'get_class_to_class_keyword' => true,
+        'mb_str_functions' => true,
+        'modernize_strpos' => true,
+        'no_useless_concat_operator' => false, // TODO switch back on when the `src/Console/Application.php` no longer needs the concat
+        'numeric_literal_separator' => true,
+        'string_implicit_backslashes' => true, // https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/pull/7786
+        'php_unit_test_case_static_method_calls' => false,
+    ])
+    ->setFinder(
+        (new Finder())
+            ->ignoreDotFiles(false)
+            ->ignoreVCSIgnored(true)
+            ->in(__DIR__.'/src')
+            ->in(__DIR__.'/tests')
+    )
+;
