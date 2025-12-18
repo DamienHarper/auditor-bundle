1676e5c2cdc2d56be1256eb85f8bb0ab4c1b3f44
Jan Klan <jan@klan.com.au>
2025-12-18 14:13:15 +1030

    Allow Symfony 8 and fix issues revealed by the test suite

--- src/Controller/ViewerController.php
+++ src/Controller/ViewerController.php
@@ -15,7 +15,7 @@ use DH\AuditorBundle\Tests\Controller\ViewerControllerTest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException as SymfonyAccessDeniedException;
 use Twig\Environment;
 
